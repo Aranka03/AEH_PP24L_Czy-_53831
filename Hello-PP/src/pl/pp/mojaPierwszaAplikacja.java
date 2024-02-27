057d8fc03cccea3db27f0e7ea6d0630cd1384bb6
Aleksandra Czyż <ola.aranka@gmail.com>
2024-02-27 13:07:38 +0100

    Pierwszy zapis do repozytorium

--- Hello-PP/src/pl/pp/mojaPierwszaAplikacja.java
+++ Hello-PP/src/pl/pp/mojaPierwszaAplikacja.java
@@ -2,7 +2,7 @@ package pl.pp;
 
 public class mojaPierwszaAplikacja {
     public static void main(String[] args) {
-        System.out.println("Witaj PP!");
+        System.out.print("Witaj PP!");
     }
 }
 
