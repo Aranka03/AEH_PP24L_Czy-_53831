3cdd2bc6a03cd3e6bfbeb7fe3927a40db10c1858
Aleksandra Czyż <ola.aranka@gmail.com>
2024-03-06 11:20:23 +0100

    Zapis do repozytorium Lab2

--- Hello-PP/src/pl/pp/zadanie2_1.java
+++ Hello-PP/src/pl/pp/zadanie2_1.java
@@ -4,8 +4,8 @@ public class zadanie2_1 {
     public static void main(String[] args) {
 
         int x = 10; // Deklaracja zmiennej x
-        int res1 = 2*x; //Obliczenie dwukrotnosci x
-        int res2 = (x*x); //Obliczenie potegi drugiego stopia x
+        int res1 = 2*x; // Obliczenie dwukrotnosci x
+        int res2 = (x*x); // Obliczenie potegi drugiego stopia x
 
         System.out.println("Liczba x = " + x);
         System.out.println("Dwukrotnosc liczby x = " + res1);
