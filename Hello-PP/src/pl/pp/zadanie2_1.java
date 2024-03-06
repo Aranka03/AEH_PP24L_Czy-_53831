df2bab2dd455c450233e0f3989373f5ff7d837ae
Aleksandra Czyż <ola.aranka@gmail.com>
2024-03-06 11:11:43 +0100

    Pierwszy zapis do repozytorium Lab2

---
+++ Hello-PP/src/pl/pp/zadanie2_1.java
@@ -0,0 +1,15 @@
+package pl.pp;
+
+public class zadanie2_1 {
+    public static void main(String[] args) {
+
+        int x = 10; // Deklaracja zmiennej x
+
+        int res1 = 2*x; //Obliczenie dwukrotnosci x
+        int res2 = (x*x); //Obliczenie potegi drugiego stopia x
+
+        System.out.println("Liczba x = " + x);
+        System.out.println("Dwukrotnosc liczby x = " + res1);
+        System.out.println("Potega drugiego stopnia z liczby x = " + res2);
+    }
+}
---
+++ Hello-PP/src/pl/pp/zadanie2_2.java
@@ -0,0 +1,19 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class zadanie2_2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swoj wiek w latach: ");
+        int ageYears = scanner.nextInt();
+
+        // Zakladam, ze rok ma 365 dni
+        int ageSec = ageYears * 365 * 24 * 60 * 60;
+
+        System.out.println("Twoj wiek w sekudach: " + ageSec);
+        scanner.close();
+    }
+}
